aa7237c0446609d789c3b2c4e259bb4f02e125ee
emil <emil.kantaneva@gmail.com>
2023-02-02 13:11:18 +0200

    added CreateBeverage and SearchBeverage handlers

--- backend/handlers/beverage_handler.go
+++ backend/handlers/beverage_handler.go
@@ -3,13 +3,16 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/emilsto/alma-task/config"
+	"github.com/gorilla/mux"
 
 	"github.com/emilsto/alma-task/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -47,6 +50,66 @@ func GetBeverages(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		respondWithError(w, "Error encoding JSON", http.StatusInternalServerError)
 		return
+	}
+
+}
+
+// Create a new beverage
+func CreateBeverage(w http.ResponseWriter, r *http.Request) {
+	var beverage models.Beverage
+
+	// Decode the request body
+	if err := json.NewDecoder(r.Body).Decode(&beverage); err != nil {
+		respondWithError(w, "Error decoding JSON", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(beverage)
+
+	// Insert the beverage into the collection
+	if _, err := collection.InsertOne(context.TODO(), beverage); err != nil {
+		fmt.Println(err.Error())
+		respondWithError(w, "Error inserting beverage", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(beverage); err != nil {
+		log.Println(err)
+		respondWithError(w, "Error encoding JSON", http.StatusInternalServerError)
+	}
+}
+
+// Search for a beverage
+func SearchBeverage(w http.ResponseWriter, r *http.Request) {
+	var beverages []models.Beverage
+
+	//set and check the params
+	params := mux.Vars(r)
+	query := params["query"]
+
+	cur, err := collection.Find(context.TODO(), bson.M{"name": bson.M{"$regex": primitive.Regex{Pattern: query, Options: "i"}}})
+	if err != nil {
+		respondWithError(w, "No beverages found!", http.StatusInternalServerError)
+		return
+	}
+	defer cur.Close(context.TODO())
+
+	for cur.Next(context.TODO()) {
+		var beverage models.Beverage
+		err := cur.Decode(&beverage)
+		if err != nil {
+			respondWithError(w, "Invalid req payload", http.StatusBadRequest)
+		}
+		beverages = append(beverages, beverage)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(beverages); err != nil {
+		log.Println(err)
+		respondWithError(w, "Error endocing JSON", http.StatusInternalServerError)
 	}
 
 }
