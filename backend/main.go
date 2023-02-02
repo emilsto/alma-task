0c9a8aea0e123472cbff35988816a5d43329a120
emil <emil.kantaneva@gmail.com>
2023-02-02 13:08:25 +0200

    added routes to main, install dependencies

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/emilsto/alma-task/config"
+	"github.com/emilsto/alma-task/routes"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
@@ -15,9 +16,13 @@ func main() {
 	r := mux.NewRouter()
 	handler := cors.Default().Handler(r)
 
+	api := r.PathPrefix("/api/v1").Subrouter()
+
 	// Connect to the database
 	config.ConnectDB()
 
+	// Set up the routes
+	routes.BeverageRoute(api)
 
 	// Start the server, listen on port 5000 and use gorilla/mux as the router
 	log.Fatal(http.ListenAndServe(":5000", handler))
