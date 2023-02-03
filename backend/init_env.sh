a6e28164bce897c7d7e8b5f2a37ad32d2134dd5b
emil <emil.kantaneva@gmail.com>
2023-02-03 14:55:42 +0200

    update init script

--- backend/init_env.sh
+++ backend/init_env.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "PORT = \":5000"\ > .env
-echo "MONGO_URI = \"vaihda minut!\"" > .env
+echo "MONGO_URI = \"vaihda minut!\"" >> .env
 echo "MONGO_COLLECTION = \"beverages\"" >> .env
 
-cd tests
 echo "PORT = \":5000"\ > tests/.env
-echo "MONGO_URI = \"vaihda minut!\"" > tests/.env
+echo "MONGO_URI = \"vaihda minut!\"" >> tests/.env
 echo "MONGO_COLLECTION = \"test_beverages\"" >> tests/.env
 
-cd ..
 echo "Init done!, remember to change the MONGO_URI in .env and tests/.env files!"
